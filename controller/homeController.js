eabbb3e59b47dd1bb0d71af2c3e4899125f9cf37
GiovannaCardins <106175075+GiovannaCardins@users.noreply.github.com>
2022-08-30 20:58:07 -0300

    Update homeController.js

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,10 +1,8 @@
 //! Cria a classe de controller:
 class HomeController{
-    //* Define o Index do Controller:
     index(req, res) {
         res.render('index');
     }
-    //* Define o Home do Controller:
     home(req, res){
         res.render('pages/home');
     }
