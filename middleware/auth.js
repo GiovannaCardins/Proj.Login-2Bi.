3eac04348ab38d7c95f19d59fe4f6c40d2b84cd4
GiovannaCardins <106175075+GiovannaCardins@users.noreply.github.com>
2022-08-30 20:57:35 -0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,10 @@
 //! Cria a função de autenticação:
 const auth = (req, res, next) => {
-    //* Verifica se a seção está logada:
     console.log(req.session.logado);
     if(req.session.logado){
-        //? Continua:
         next();
     }
     else{
-        //* Redireciona para a página de login se não tiver sido logado:
         return res.redirect('/login');
     }
 }
